31418bb1f50489cf615e723657e51ad5937a2d7e
Kisho Linux <kisokumar886@gmail.com>
2022-02-24 22:42:31 +0000

    exclude function working

--- wordleSolver.py
+++ wordleSolver.py
@@ -2,12 +2,28 @@ import sys
 
 wordsPath = sys.argv[1]
 
-with open(wordsPath,'r') as possibleWords:
-    possibleWords = possibleWords.read()
-
-
-print(possibleWords)
 
 
 
+def wordleSolver(exclude):
+    with open(wordsPath,'r') as possibleWords:
+        possibleWords = possibleWords.read()
+    possible = True
+    possibleWords = list(possibleWords.split())
 
+    exc = [c for c in exclude]
+
+    for word in possibleWords:
+
+        for i in exc:
+            if i not in word:
+                possible = False
+        
+        if possible:
+            print(word)
+     
+
+wordleSolver('abc')
+
+# input = abc
+# output = aback
