f347acbf673cace52b8652946fdbe2f25ac498a6
Kisho Linux <kisokumar886@gmail.com>
2022-02-26 08:28:06 +0000

    added the ability to sort through correct position (green letters) words

--- wordleSolver.py
+++ wordleSolver.py
@@ -1,7 +1,7 @@
 import sys
 wordsPath = sys.argv[1]
 
-def wordleSolver(exclude):
+def wordleSolver(exclude,correctPosition):
     with open(wordsPath,'r') as possibleWords:
         possibleWords = possibleWords.read()
     
@@ -10,6 +10,7 @@ def wordleSolver(exclude):
     exc = [c for c in exclude]
 
     for word in possibleWords:
+        temp = [c for c in word]
         possible = True
 
         for i in exc:
@@ -17,10 +18,19 @@ def wordleSolver(exclude):
                 possible = False
                 break
         
+        for i,j in enumerate(correctPosition):
+            if j!='':
+                if temp[i]!=j:
+                    possible = False
+
+
         if possible:
             print(word)
      
-wordleSolver('acdefjklnqrstuvwy')
+wordleSolver('elo',['a','','n','',''])
 
-# input = acdefjklnqrstuvwy
-# output = hippo
+# wordleSolver(incorrect letters,[correct letters])
+# wordleSolver(grey letters,[list of green letters])
+
+# input = 'elo',['a','','n','','']
+# output = annul,aunty
