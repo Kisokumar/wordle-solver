e1c945ebb2f0a1f6822d0c6e2ea47993cf6574fe
Kisho Linux <kisokumar886@gmail.com>
2022-03-04 20:26:29 +0000

    filters for words with letters in the correct & incorrect positions

--- wordleSolver.py
+++ wordleSolver.py
@@ -1,37 +1,45 @@
 import sys
 wordsPath = sys.argv[1]
 
-def wordleSolver(exclude,correctPosition):
-    global results
-    with open(wordsPath,'r') as possibleWords:
+def wordleSolver(exclude,correctPosition,wrongPosition,myLexicon):
+    results = []
+    exc =[c for c in exclude]
+ 
+    with open(myLexicon,'r') as possibleWords:
         possibleWords = possibleWords.read()
-    
-    
-    possibleWords = list(possibleWords.split())
-    exc = [c for c in exclude]
+        possibleWords = list(possibleWords.split())
 
     for word in possibleWords:
         temp = [c for c in word]
         correctWord = True
-
         for i in exc:
-            if i in word:
-                correctWord = False
-                break
-        
+            if i not in correctPosition and i not in wrongPosition:
+                if i in word:
+                    correctWord = False
+
         for i,j in enumerate(correctPosition):
             if j!='':
                 if temp[i]!=j:
                     correctWord = False
 
-
+        for i,j in enumerate(wrongPosition):
+            if j!='':
+                for z in j:
+                    if z not in temp:
+                        correctWord = False
+                    if temp[i]==z:
+                        correctWord = False
+        
         if correctWord:
-            print(word)
-     
-wordleSolver('elo',['a','','n','',''])
+            results.append(word)
+    
+    print('Number of possible words: ',len(results))
+    print(results)
 
-# wordleSolver(incorrect letters,[correct letters])
-# wordleSolver(grey letters,[list of green letters])
+exc,correctPosition,wrongPosition='elo',['a','','n','',''],['','','','','']
+wordleSolver(exc,correctPosition,wrongPosition,wordsPath)
+# wordleSolver(incorrect letters,[correct position],[incorrect position], /path/to/words)
+# wordleSolver(grey letters,[list of green letters],[list of yellow letters], /path/to/words)
 
-# input = 'elo',['a','','n','','']
-# output = annul,aunty
+# input = 'eo',['a','','n','',''],['','','','','']
+# output = aunty
